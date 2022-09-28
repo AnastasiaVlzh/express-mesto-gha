d59a2f545492fb1a38703b01009c7d9dfcbfb749
Anastasia Volzhina <happy-heart@bk.ru>
2022-09-28 22:12:57 +0300

    15PR

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const { celebrate, Joi, errors } = require('celebrate');
 require('dotenv').config();
 const userRoutes = require('./routes/user');
@@ -12,9 +13,16 @@ const {
 const { auth } = require('./middlewares/auth');
 const NotFoundError = require('./errors/not-found-err');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 4000 } = process.env;
 
 const app = express();
+
+app.use(
+  cors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+  }),
+);
 
 app.use(cookieParser());
 
